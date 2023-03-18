6cecfe68b54465d3c06cbef75ede1b497056d275
AndresPM98 <andresperez_98@hotmail.com>
2023-03-17 22:29:50 -0300

    sckroll

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import style  from './navbar.module.css';
+import style from './navbar.module.css';
 
-const Navbar = () => {
-    return(
-        
-        <div className={style.navbar}>
-            <button className={style.bottAbout}>About</button>
-            <button className={style.bottSkills}>Skills</button>
-            <button className={style.bottLabs}>Labs</button>
-            <button className={style.bottContact}>Contact</button>
-        </div>
-
-    )
-}
+const Navbar = ({ aboutRef, skillsRef, labsRef, contactRef, scrollToSection }) => {
+  return (
+    <div className={style.navbar}>
+      <button className={style.bottAbout} onClick={() => scrollToSection(aboutRef)}>
+        About
+      </button>
+      <button className={style.bottSkills} onClick={() => scrollToSection(skillsRef)}>
+        Skills
+      </button>
+      <button className={style.bottLabs} onClick={() => scrollToSection(labsRef)}>
+        Labs
+      </button>
+      <button className={style.bottContact} onClick={() => scrollToSection(contactRef)}>
+        Contact
+      </button>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import About from '../../components/about/about';
 import Contact from '../../components/contact/contact';
 import Labs from '../../components/labs/labs';
 import Navbar from '../../components/navbar/navbar';
 import Skills from '../../components/skills/skills';
-import style from "./home.module.css";
-
+import style from './home.module.css';
 
 const Home = () => {
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const labsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
       <div className={style.navbarCont}>
-        <Navbar/>
+        <Navbar
+          aboutRef={aboutRef}
+          skillsRef={skillsRef}
+          labsRef={labsRef}
+          contactRef={contactRef}
+          scrollToSection={scrollToSection}
+        />
       </div>
-    <div className={style.aboutCont}>
-      <About/>
+      <div className={style.aboutCont} ref={aboutRef}>
+        <About />
+      </div>
+      <div className={style.skillsCont} ref={skillsRef}>
+        <Skills />
+      </div>
+      <div className={style.labsCont} ref={labsRef}>
+        <Labs />
+      </div>
+      <div className={style.contactCont} ref={contactRef}>
+        <Contact />
+      </div>
     </div>
-    <div className={style.skillsCont}>
-      <Skills/>
-    </div>
-    <div className={style.labsCont}>
-      <Labs/>
-    </div>
-    <div className={style.contactCont}>
-      <Contact/>
-    </div>
-  </div>
   );
 };
 
 export default Home;
-
-
-
-
-
-
-
