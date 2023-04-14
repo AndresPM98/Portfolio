4c2cc9d665ed36c902afc09bdaed6c202479e7e9
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-04-13 21:57:06 -0300

    cambios generales, texto

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import style from "./landing.module.css";
 import imgLanding from "../../img/imgLanding.png";
+/* import mail from "../../img/mail.png";
+import linkedin from "../../img/linkedin.png";
+import githube from "../../img/githube.png";
+import wpp from "../../img/wpp.png"; */
 
 const Landing = () => {
- 
   return (
     <>
       <div className={style.container}>
         <div className={style.imgLanding}>
           <img className={style.imgLand} src={imgLanding} alt="Imagen" />
+          <div className={style.line}></div>
         </div>
         <h1 className={style.h1Landing}>Andrés Pérez Merello - WebDev</h1>
         <p className={style.pLanding}>
-          JavaScript / HTML / CSS / React / Redux / PostgreSQL / Express / Node 
+          JavaScript / HTML / CSS / React / Redux / PostgreSQL / Express / Node
         </p>
-        <div className={style.line}></div>
+        
+       {/*  <div className={style.iconos}>
+          <img className={style.mail} height="50px" src={mail} alt="mail" />
+          <img
+            className={style.linkedin}
+            height="50px"
+            src={linkedin}
+            alt="linkedin"
+          />
+          <img
+            className={style.githube}
+            height="50px"
+            src={githube}
+            alt="githube"
+          />
+          <img className={style.wpp} height="50px" src={wpp} alt="wpp" />
+        </div> */}
       </div>
     </>
   );
