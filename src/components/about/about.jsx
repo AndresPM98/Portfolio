5dcdee366a41bb312f364cd9d0d595dfac8aabd4
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-04-14 13:28:55 -0300

    img abaoutMe

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Text from "../text/text";
-import style from "./about.module.css"
-import aboutMe2 from "../../img/aboutMe2.png"
+import Yo from "../../img/yo.png";
+import style from "./about.module.css";
+
 
 const About = () => {
   return (
     <div>
-        <Text />
+      <Text />
+      <div className={style.recuadro}></div>
+      <div className={style.recuadro2}></div>
+      <div className={style.imgYo}>
+        <img src={Yo} alt="Yo" />
+      </div>
     </div>
   );
 };
