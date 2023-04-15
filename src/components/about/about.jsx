493a02103c349ebb5cd733ffa7e25ea725464c04
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-04-15 13:08:33 -0300

    foto mia

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,15 +3,16 @@ import Text from "../text/text";
 import Yo from "../../img/yo.png";
 import style from "./about.module.css";
 
-
 const About = () => {
   return (
     <div>
       <Text />
-      <div className={style.recuadro}></div>
-      <div className={style.recuadro2}></div>
-      <div className={style.imgYo}>
-        <img src={Yo} alt="Yo" />
+      <div className={style.recuadro}>
+        <div className={style.recuadro2}>
+          <div className={style.imgYo}>
+            <img src={Yo} alt="Yo" />
+          </div>
+        </div>
       </div>
     </div>
   );
