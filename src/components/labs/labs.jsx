2f8ef86a67ff6fe4f3d4e5f1e60bb7fead52d037
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-04-15 12:06:05 -0300

    arreglos generales labs y cards labs

--- src/components/labs/labs.jsx
+++ src/components/labs/labs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FlexAg from "../../img/FlexAg.png";
 import Henry from "../../img/henryLogo.png";
+import Pato from "../../img/PATOimg.png"
 import style from "./labs.module.css";
 
 const Labs = () => {
@@ -35,8 +36,8 @@ const Labs = () => {
       </div>
 
       <div class={style.card}>
-        <div class={style.image}>
-          <img src={Henry} alt="Henry" />
+        <div class={style.imagePato}>
+          <img src={Pato} alt="Pato" />
         </div>
         <div class={style.card_description}>
           <p class={style.text_title}> Proyecto Freelacer</p>
@@ -46,6 +47,19 @@ const Labs = () => {
         </div>
       </div>
 
+      <div class={style.card}>
+        <div class={style.image}>
+          <img src={Henry} alt="Henry" />
+        </div>
+        <div class={style.card_description}>
+          <p class={style.text_title}> Proyecto Grupal</p>
+          <p class={style.text_body}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor.
+          </p>
+        </div>
+      </div>
+      
       </div>
     </div>
   );
